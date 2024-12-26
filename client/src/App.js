412cc4b8f70d46963a44b31f207cc4773a8579aa
Shyam2397 <shyamsundarr10@gmail.com>
2024-12-27 00:37:46 +0530

    update the mainlayout path

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
-import MainLayout from "./components/MainLayout";
+import MainLayout from "./components/mainLayout/MainLayout";
 import Dashboard from "./pages/Dashboard";
 import NewEntries from "./pages/NewEntries";
 import SkinTesting from "./pages/SkinTesting";
