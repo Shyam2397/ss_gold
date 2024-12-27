67d5f77d24c8e67f381a6a40a09f89d157eb3c70
Shyam2397 <shyamsundarr10@gmail.com>
2024-12-27 18:54:06 +0530

    update dashboard path

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import MainLayout from "./components/mainLayout/MainLayout";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import NewEntries from "./pages/NewEntries";
 import SkinTesting from "./pages/SkinTesting";
 import PhotoTesting from "./pages/PhotoTesting";
