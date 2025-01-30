c4d18e340f5800c16af3cd25166470100ebbaf74
Shyam2397 <shyamsundarr10@gmail.com>
2025-01-30 22:03:25 +0530

    review the mainlayout

--- client/src/components/mainLayout/MainContent.jsx
+++ client/src/components/mainLayout/MainContent.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import CurveTransition from '../transitions/CurveTransition';
 
 const MainContent = () => {
-  
-  const location = useLocation();
-  
   return (
-    <main className="flex-1 min-h-screen bg-gradient-to-br from-amber-50 to-yellow-100 overflow-auto">
+    <main className="flex-1 min-h-screen bg-gradient-to-br from-amber-50 to-yellow-100 overflow-y-auto">
       <AnimatePresence mode="wait">
-        <CurveTransition key={location.pathname} />
+        <CurveTransition />
       </AnimatePresence>
       <div className="container mx-auto h-full">
         <Outlet />
--- client/src/components/mainLayout/MainLayout.jsx
+++ client/src/components/mainLayout/MainLayout.jsx
@@ -1,32 +1,28 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import Sidebar from '../sidebar/Sidebar';
 import MainContent from './MainContent';
 
 const MainLayout = ({ setLoggedIn }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const location = useLocation();
-  
+
   const user = {
     name: "John Doe",
     profileImage: "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp",
   };
 
   return (
-    <div className="flex h-screen bg-gray-50">
-      {/* Sidebar */}
+    <div className="flex flex-col md:flex-row h-screen bg-gray-50 overflow-hidden">
       <Sidebar 
         open={isSidebarOpen}
         setOpen={setIsSidebarOpen}
         animate={true}
         user={user}
         setLoggedIn={setLoggedIn}
+        className="w-full md:w-64 lg:w-72 xl:w-80"
       />
-
-      {/* Main Content */}
       <div className="flex-1 flex flex-col overflow-hidden">
-        <div className="flex-1 overflow-auto">
-          <MainContent key={location.pathname} />
+        <div className="flex-1 overflow-y-auto">
+          <MainContent />
         </div>
       </div>
     </div>
