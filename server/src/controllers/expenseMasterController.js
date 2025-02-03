6aec850680ea49fe207a212eb965fd089d537f2f
Shyam2397 <shyamsundarr10@gmail.com>
2025-02-03 21:57:19 +0530

    update pool

--- server/src/controllers/expenseMasterController.js
+++ server/src/controllers/expenseMasterController.js
@@ -1,4 +1,4 @@
-const pool = require('../config/database');
+const { pool } = require('../config/database');
 
 // Create a new expense type
 const createExpenseType = (req, res) => {
--- server/src/controllers/pureExchangeController.js
+++ server/src/controllers/pureExchangeController.js
@@ -1,4 +1,4 @@
-const pool = require('../config/database');
+const { pool } = require('../config/database');
 
 const getAllPureExchanges = async (req, res) => {
   const sql = 'SELECT * FROM pure_exchange';
