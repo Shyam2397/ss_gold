0757f8145a229bbf7b53582c3641a08abaabb699
Shyam2397 <shyamsundarr10@gmail.com>
2025-02-16 20:37:49 +0530

    improve the error style

--- client/src/pages/ExchangeData/ExchangeDataPage.jsx
+++ client/src/pages/ExchangeData/ExchangeDataPage.jsx
@@ -27,23 +27,23 @@ const ExchangeDataPage = () => {
         <h1 className="text-3xl font-bold mb-4 sm:mb-0 bg-clip-text text-transparent bg-gradient-to-r from-[#391145] to-[#D3B04D]">
           Exchange Data
         </h1>
-        <ExportButton data={filteredExchanges} />
-      </div>
-
-      {error && (
-        <div className="mb-4 p-3 bg-red-50 border-l-4 border-red-500 text-red-700 rounded-r flex items-center space-x-2 text-sm">
+        {error && (
+        <div className=" p-1 bg-red-50 border-l-4 border-red-500 text-red-700 rounded-r flex items-center space-x-2 text-sm">
           <FiAlertCircle className="h-4.5 w-4.5" />
           <span>{error}</span>
         </div>
       )}
-
+      
       {successMessage && (
-        <div className="mb-4 p-3 bg-green-50 border-l-4 border-green-500 text-green-700 rounded-r flex items-center space-x-2 text-sm">
+        <div className="p-1 bg-green-50 border-l-4 border-green-500 text-green-700 rounded-r flex items-center space-x-2 text-sm">
           <FiAlertCircle className="h-4.5 w-4.5" />
           <span>{successMessage}</span>
         </div>
       )}
+        <ExportButton data={filteredExchanges} />
+      </div>
 
+    
       <div className="mb-5 bg-white p-5 rounded-lg shadow-sm">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 items-end">
           <DateInput
